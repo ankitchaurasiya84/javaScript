7f45c2d7ccdff6618dc51e91639c0515d408a772
Ankit Chaurasiya <ankitchaurasiya84@Ankits-MacBook-Air.local>
2025-05-12 19:03:13 +0530

    commit

---
+++ clouser/data.js
@@ -0,0 +1,30 @@
+const data=()=>{
+    let a=10;
+    let b=20;
+    const sum=()=>{
+         console.log(a+b);
+    }
+    return sum
+}
+
+data()() 
+
+
+// data() is a function that returns another function (sum).
+// The outer function (data) creates a closure that captures the variables a and b.
+// The inner function (sum) has access to the variables a and b even after the outer function has finished executing.
+// This is an example of a closure in JavaScript.
+
+
+// currying
+
+const Curring=(a)=>{
+    let c;
+    const sum1=(b)=>{
+        let d;
+         console.log(a+b);
+    }
+    return sum1
+}
+
+Curring(12)(12) // 24
